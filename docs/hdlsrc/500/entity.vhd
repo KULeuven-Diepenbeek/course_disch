8c5884a9a83e31e61c15dfc4e1845b6fcfaf59c5
Jo Vliegen <jo.vliegen@kuleuven.be>
2026-03-13 15:02:59 +0100

    publishing

--- docs/hdlsrc/500/entity.vhd
+++ docs/hdlsrc/500/entity.vhd
@@ -1,8 +1,14 @@
 entity processor is
+    generic(
+        G_DATAWIDTH : natural := 32;
+        G_REGCOUNT_LOG2 : natural := 5
+    );
     port(
         clock : in STD_LOGIC;
         reset : in STD_LOGIC;
-        pc : out STD_LOGIC_VECTOR(31 downto 0);
-        inst : in STD_LOGIC_VECTOR(31 downto 0)
+        inst : in STD_LOGIC_VECTOR(G_DATAWIDTH-1 downto 0);
+        pc : out STD_LOGIC_VECTOR(G_DATAWIDTH-1 downto 0);
+        data_out : out STD_LOGIC_VECTOR(G_DATAWIDTH-1 downto 0);
+        data_out_we : out STD_LOGIC
     );
 end entity processor;
